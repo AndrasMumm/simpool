e85409ec6951106091e256931f06ff74b8ba8a6f
Magxm <19425588+Magxm@users.noreply.github.com>
2020-12-16 19:08:42 +0100

    Blocklist from Linked List to Double Linked List, added Hashmap to keep track of allocated blocks to increase free speed

--- DynamicSizePool.hpp
+++ DynamicSizePool.hpp
@@ -1,8 +1,13 @@
 #ifndef _DYNAMICSIZEPOOL_HPP
 #define _DYNAMICSIZEPOOL_HPP
 
+#include <algorithm>
 #include <cstddef>
 #include <cassert>
+#include <iostream>
+#include <map>
+#include <mutex>
+
 #include "StdAllocator.hpp"
 #include "FixedSizePool.hpp"
 
@@ -10,224 +15,341 @@ template <class MA, class IA = StdAllocator>
 class DynamicSizePool
 {
 protected:
-  struct Block
-  {
-    char *data;
-    std::size_t size;
-    bool isHead;
-    Block *next;
-  };
+	struct Block
+	{
+		char* data;
+		std::size_t size;
+		bool isHead;
+		Block* next;
+		Block* prev;
+	};
 
-  // Allocator for the underlying data
-  typedef FixedSizePool<struct Block, IA, IA, (1<<6)> BlockPool;
-  BlockPool blockPool;
+	// Allocator for the underlying data
+	typedef FixedSizePool<struct Block, IA, IA, (1 << 6)> BlockPool;
+	BlockPool blockPool;
 
-  // Start of the nodes of used and free block lists
-  struct Block *usedBlocks;
-  struct Block *freeBlocks;
+	// Start of the nodes of used and free block lists
+	struct Block* usedBlocks;
+	struct Block* freeBlocks;
 
-  // Total size allocated (bytes)
-  std::size_t totalBytes;
+	// Total size allocated (bytes)
+	std::size_t totalBytes;
 
-  // Allocated size (bytes)
-  std::size_t allocBytes;
+	// Allocated size (bytes)
+	std::size_t allocBytes;
 
-  // Minimum size for allocations
-  std::size_t minBytes;
+	// Minimum size for allocations
+	std::size_t minBytes;
 
-  // Search the list of free blocks and return a usable one if that exists, else NULL
-  void findUsableBlock(struct Block *&best, struct Block *&prev, std::size_t size) {
-    best = prev = NULL;
-    for ( struct Block *iter = freeBlocks, *iterPrev = NULL ; iter ; iter = iter->next ) {
-      if ( iter->size >= size && (!best || iter->size < best->size) ) {
-        best = iter;
-        prev = iterPrev;
-      }
-      iterPrev = iter;
-    }
-  }
+	// Search the list of free blocks and return a usable one if that exists, else NULL
+	void findUsableBlock(struct Block*& best, struct Block*& prev, std::size_t size) {
+		best = prev = NULL;
+		for (struct Block* iter = freeBlocks, *iterPrev = NULL; iter; iter = iter->next) {
+			if (iter->size >= size && (!best || iter->size < best->size)) {
+				best = iter;
+				prev = iterPrev;
+			}
+			iterPrev = iter;
+		}
+	}
+#undef max
+#undef min
 
-  // Allocate a new block and add it to the list of free blocks
-  void allocateBlock(struct Block *&curr, struct Block *&prev, const std::size_t size) {
-    const std::size_t sizeToAlloc = std::max(size, minBytes);
-    curr = prev = NULL;
-    void *data = NULL;
+	// Allocate a new block and add it to the list of free blocks
+	void allocateBlock(struct Block*& curr, struct Block*& prev, const std::size_t size) {
+		const std::size_t sizeToAlloc = std::max(size, minBytes);
+		curr = prev = NULL;
+		void* data = NULL;
 
-    // Allocate data
-    data = MA::allocate(sizeToAlloc);
-    totalBytes += sizeToAlloc;
-    assert(data);
+		// Allocate data
+		data = MA::allocate(sizeToAlloc);
+		totalBytes += sizeToAlloc;
+		assert(data);
 
-    // Find next and prev such that next->data is still smaller than data (keep ordered)
-    struct Block *next;
-    for ( next = freeBlocks; next && next->data < data; next = next->next ) {
-      prev = next;
-    }
+		// Find next and prev such that next->data is still smaller than data (keep ordered)
+		struct Block* next;
+		for (next = freeBlocks; next && next->data < data; next = next->next) {
+			prev = next;
+		}
 
-    // Allocate the block
-    curr = (struct Block *) blockPool.allocate();
-    if (!curr) return;
-    curr->data = static_cast<char *>(data);
-    curr->size = sizeToAlloc;
-    curr->isHead = true;
-    curr->next = next;
+		// Allocate the block
+		curr = (struct Block*)blockPool.allocate();
+		if (!curr) return;
+		curr->data = static_cast<char*>(data);
+		curr->size = sizeToAlloc;
+		curr->isHead = true;
+		curr->next = next;
+		if (next)
+		{
+			next->prev = curr;
+		}
 
-    // Insert
-    if (prev) prev->next = curr;
-    else freeBlocks = curr;
-  }
 
-  void splitBlock(struct Block *&curr, struct Block *&prev, const std::size_t size) {
-    struct Block *next;
-    if ( curr->size == size ) {
-      // Keep it
-      next = curr->next;
-    }
-    else {
-      // Split the block
-      std::size_t remaining = curr->size - size;
-      struct Block *newBlock = (struct Block *) blockPool.allocate();
-      if (!newBlock) return;
-      newBlock->data = curr->data + size;
-      newBlock->size = remaining;
-      newBlock->isHead = false;
-      newBlock->next = curr->next;
-      next = newBlock;
-      curr->size = size;
-    }
+		// Insert
+		if (prev)
+		{
+			prev->next = curr;
+			curr->prev = prev;
+		}
+		else
+		{
+			freeBlocks = curr;
+		}
+	}
 
-    if (prev) prev->next = next;
-    else freeBlocks = next;
-  }
+	void splitBlock(struct Block*& curr, struct Block*& prev, const std::size_t size) {
+		struct Block* next;
+		if (curr->size == size) {
+			// Keep it
+			next = curr->next;
+		}
+		else {
+			// Split the block, curr->size is bigger than size
+			//Calculating new size
+			std::size_t remaining = curr->size - size;
+			//allocing new block
+			struct Block* newBlock = (struct Block*)blockPool.allocate();
+			if (!newBlock) return;
+			//Splitting current blocks data, newBlock will be the new one while current maintains it's size
+			newBlock->data = curr->data + size;
+			newBlock->size = remaining;
+			newBlock->isHead = false;
+			//Since we will remove this block from the freelist, we basically replace it with the newBlock
+			newBlock->next = curr->next;
+			next = newBlock;
+			//Updating current blocks size
+			curr->size = size;
+		}
 
-  void releaseBlock(struct Block *curr, struct Block *prev) {
-    assert(curr != NULL);
+		if (prev)
+		{
+			//Updating prev references
+			prev->next = next;
+			if (next)
+			{
+				next->prev = prev;
+			}
+		}
+		else
+		{
+			freeBlocks = next;
+		}
+	}
 
-    if (prev) prev->next = curr->next;
-    else usedBlocks = curr->next;
+	void releaseBlock(struct Block* curr, struct Block* prev) {
+		assert(curr != NULL);
 
-    // Find location to put this block in the freeBlocks list
-    prev = NULL;
-    for ( struct Block *temp = freeBlocks ; temp && temp->data < curr->data ; temp = temp->next ) {
-      prev = temp;
-    }
+		if (prev)
+		{
+			//Cutting block out of linked list chain.
+			prev->next = curr->next;
+			if (curr->next)
+			{
+				curr->next->prev = prev;
+			}
+		}
+		else
+		{
+			usedBlocks = curr->next;
+			if (curr->next)
+			{
+				curr->next->prev = nullptr;
+			}
+			if (usedBlocks)
+			{
+				usedBlocks->prev = nullptr;
+			}
+		}
 
-    // Keep track of the successor
-    struct Block *next = prev ? prev->next : freeBlocks;
+		// Find location to put this block in the freeBlocks list
+		prev = NULL;
+		for (struct Block* temp = freeBlocks; temp && temp->data < curr->data; temp = temp->next) {
+			prev = temp;
+		}
 
-    // Check if prev and curr can be merged
-    if ( prev && prev->data + prev->size == curr->data && !curr->isHead ) {
-      prev->size = prev->size + curr->size;
-      blockPool.deallocate(curr); // keep data
-      curr = prev;
-    }
-    else if (prev) {
-      prev->next = curr;
-    }
-    else {
-      freeBlocks = curr;
-    }
+		// Keep track of the successor
+		struct Block* next = prev ? prev->next : freeBlocks;
 
-    // Check if curr and next can be merged
-    if ( next && curr->data + curr->size == next->data && !next->isHead ) {
-      curr->size = curr->size + next->size;
-      curr->next = next->next;
-      blockPool.deallocate(next); // keep data
-    }
-    else {
-      curr->next = next;
-    }
-  }
+		// Check if prev and curr can be merged
+		if (prev && prev->data + prev->size == curr->data && !curr->isHead) {
+			//We can merge both of the blocks.
+			prev->size = prev->size + curr->size;
+			blockPool.deallocate(curr); // keeps data
+			curr = prev;
+		}
+		else if (prev) {
+			prev->next = curr;
+			curr->prev = prev;
+		}
+		else {
+			freeBlocks = curr;
+			if (curr->prev)
+			{
+				curr->prev = nullptr;
+			}
+		}
 
-  void freeAllBlocks() {
-    // Release the used blocks
-    while(usedBlocks) {
-      releaseBlock(usedBlocks, NULL);
-    }
+		// Check if curr and next can be merged
+		if (next && curr->data + curr->size == next->data && !next->isHead) {
+			curr->size = curr->size + next->size;
+			curr->next = next->next;
+			if (next->next)
+			{
+				next->next->prev = curr;
+			}
+			blockPool.deallocate(next); // keep data
+		}
+		else {
+			curr->next = next;
+			if (next)
+			{
+				next->prev = curr;
+			}
+		}
+	}
 
-    // Release the unused blocks
-    while(freeBlocks) {
-      assert(freeBlocks->isHead);
-      MA::deallocate(freeBlocks->data);
-      totalBytes -= freeBlocks->size;
-      struct Block *curr = freeBlocks;
-      freeBlocks = freeBlocks->next;
-      blockPool.deallocate(curr);
-    }
-  }
+	void freeAllBlocks() {
+		// Release the used blocks
+		while (usedBlocks) {
+			releaseBlock(usedBlocks, NULL);
+		}
+
+		// Release the unused blocks
+		while (freeBlocks) {
+			assert(freeBlocks->isHead);
+			MA::deallocate(freeBlocks->data);
+			totalBytes -= freeBlocks->size;
+			struct Block* curr = freeBlocks;
+			freeBlocks = freeBlocks->next;
+			blockPool.deallocate(curr);
+		}
+	}
+
+	std::mutex mutex;
+	std::map<void*, Block*> allocatedAddressToBlock;
 
 public:
-  static inline DynamicSizePool &getInstance() {
-    static DynamicSizePool instance;
-    return instance;
-  }
+	static inline DynamicSizePool* getInstance() {
+		static DynamicSizePool instance;
+		return &instance;
+	}
 
-  DynamicSizePool(const std::size_t _minBytes = (1 << 8))
-    : blockPool(),
-      usedBlocks(NULL),
-      freeBlocks(NULL),
-      totalBytes(0),
-      allocBytes(0),
-      minBytes(_minBytes) { }
+	DynamicSizePool(const std::size_t _minBytes = (1 << 8))
+		: blockPool(),
+		usedBlocks(NULL),
+		freeBlocks(NULL),
+		totalBytes(0),
+		allocBytes(0),
+		minBytes(_minBytes) { }
 
-  ~DynamicSizePool() { freeAllBlocks(); }
+	~DynamicSizePool() { freeAllBlocks(); }
 
-  void *allocate(std::size_t size) {
-    struct Block *best, *prev;
-    findUsableBlock(best, prev, size);
+	void* allocate(std::size_t size) {
+		mutex.lock();
+		struct Block* best, * prev;
+		findUsableBlock(best, prev, size);
 
-    // Allocate a block if needed
-    if (!best) allocateBlock(best, prev, size);
-    assert(best);
+		// Allocate a block if needed
+		if (!best) allocateBlock(best, prev, size);
+		assert(best);
 
-    // Split the free block
-    splitBlock(best, prev, size);
+		// Split the free block
+		splitBlock(best, prev, size);
 
-    // Push node to the list of used nodes
-    best->next = usedBlocks;
-    usedBlocks = best;
+		// Push node to the list of used nodes
+		best->next = usedBlocks;
+		if (usedBlocks)
+		{
+			usedBlocks->prev = best;
+		}
+		usedBlocks = best;
+		usedBlocks->prev = nullptr;
 
-    // Increment the allocated size
-    allocBytes += size;
+		// Increment the allocated size
+		allocBytes += size;
 
-    // Return the new pointer
-    return usedBlocks->data;
-  }
+		auto ptr = usedBlocks->data;
+		allocatedAddressToBlock.insert({ ptr, usedBlocks });
+		mutex.unlock();
+		// Return the new pointer
+		return ptr;
+	}
 
-  void deallocate(void *ptr) {
-    assert(ptr);
+	void deallocate(void* ptr) {
+		mutex.lock();
+		assert(ptr);
 
-    // Find the associated block
-    struct Block *curr = usedBlocks, *prev = NULL;
-    for ( ; curr && curr->data != ptr; curr = curr->next ) {
-      prev = curr;
-    }
-    if (!curr) return;
+		// Find the associated block
+		/*
+		struct Block* curr = usedBlocks, * prev = NULL;
+		for (; curr && curr->data != ptr; curr = curr->next) {
+			prev = curr;
+		}
+		if (!curr)
+		{
+			mutex.unlock();
+			return;
+		}
+		*/
+		auto it = allocatedAddressToBlock.find(ptr);
+		if (it == allocatedAddressToBlock.end())
+		{
+			mutex.unlock();
+			return;
+		}
 
-    // Remove from allocBytes
-    allocBytes -= curr->size;
+		struct Block* curr = it->second;
+		/*
+		if (curr != curr_)
+		{
+			std::cout << Format("curr %p != curr_ %p", prev, curr_) << std::endl;
+		}
+		else if (prev_ != prev)
+		{
+			std::cout << Format("prev %p != prev_ %p", prev, prev_) << std::endl;
+		}
+		*/
 
-    // Release it
-    releaseBlock(curr, prev);
-  }
+		allocatedAddressToBlock.erase(it);
 
-  std::size_t allocatedSize() const { return allocBytes; }
+		// Remove from allocBytes
+		allocBytes -= curr->size;
 
-  std::size_t totalSize() const {
-    return totalBytes + blockPool.totalSize();
-  }
+		// Release it
+		releaseBlock(curr, curr->prev);
+		mutex.unlock();
+	}
 
-  std::size_t numFreeBlocks() const {
-    std::size_t nb = 0;
-    for (struct Block *temp = freeBlocks; temp; temp = temp->next) nb++;
-    return nb;
-  }
+	std::size_t allocatedSize()
+	{
+		mutex.lock();
+		auto ret = allocBytes;
+		mutex.unlock();
+		return ret;
+	}
 
-  std::size_t numUsedBlocks() const {
-    std::size_t nb = 0;
-    for (struct Block *temp = usedBlocks; temp; temp = temp->next) nb++;
-    return nb;
-  }
+	std::size_t totalSize() {
+		mutex.lock();
+		auto ret = totalBytes + blockPool.totalSize();
+		mutex.unlock();
+		return ret;
+	}
+
+	std::size_t numFreeBlocks() {
+		mutex.lock();
+		std::size_t nb = 0;
+		for (struct Block* temp = freeBlocks; temp; temp = temp->next) nb++;
+		mutex.unlock();
+		return nb;
+	}
+
+	std::size_t numUsedBlocks() {
+		mutex.lock();
+		std::size_t nb = 0;
+		for (struct Block* temp = usedBlocks; temp; temp = temp->next) nb++;
+		mutex.unlock();
+		return nb;
+	}
 
 };
 
--- FixedSizePool.hpp
+++ FixedSizePool.hpp
@@ -3,144 +3,175 @@
 
 #include <cstring>
 #define  _XOPEN_SOURCE_EXTENDED 1
-#include <strings.h>
+#include <string.h>
 #include <iostream>
 #include <stdio.h>
+#include <windows.h>
+
 #include "StdAllocator.hpp"
 
-template<class T, class MA, class IA = StdAllocator, int NP=(1<<6)>
+template<class T, class MA, class IA = StdAllocator, int NP = (1 << 8)>
 class FixedSizePool
 {
 protected:
-  struct Pool
-  {
-    unsigned char *data;
-    unsigned int *avail;
-    unsigned int numAvail;
-    struct Pool* next;
-  };
+	struct Pool
+	{
+		unsigned char* data;
+		unsigned int* avail;
+		unsigned int numAvail;
+		struct Pool* next;
+	};
 
-  struct Pool *pool;
-  const std::size_t numPerPool;
-  const std::size_t totalPoolSize;
+	struct Pool* pool;
+	const std::size_t numPerPool;
+	const std::size_t totalPoolSize;
 
-  std::size_t numBlocks;
+	std::size_t numBlocks;
 
-  void newPool(struct Pool **pnew) {
-    struct Pool *p = static_cast<struct Pool *>(IA::allocate(sizeof(struct Pool) + NP * sizeof(unsigned int)));
-    p->numAvail = numPerPool;
-    p->next = NULL;
+	void newPool(struct Pool** pnew) {
+		struct Pool* p = static_cast<struct Pool*>(IA::allocate(sizeof(struct Pool) + NP * sizeof(unsigned int)));
+		p->numAvail = numPerPool;
+		p->next = NULL;
 
-    p->data  = reinterpret_cast<unsigned char*>(MA::allocate(numPerPool * sizeof(T)));
-    p->avail = reinterpret_cast<unsigned int *>(p + 1);
-    for (int i = 0; i < NP; i++) p->avail[i] = -1;
+		p->data = reinterpret_cast<unsigned char*>(MA::allocate(numPerPool * sizeof(T)));
+		p->avail = reinterpret_cast<unsigned int*>(p + 1);
+		for (int i = 0; i < NP; i++) p->avail[i] = -1;
 
-    *pnew = p;
-  }
-
-  T* allocInPool(struct Pool *p) {
-    if (!p->numAvail) return NULL;
-
-    for (int i = 0; i < NP; i++) {
-      const int bit = ffs(p->avail[i]) - 1;
-      if (bit >= 0) {
-        p->avail[i] ^= 1 << bit;
-        p->numAvail--;
-        const int entry = i * sizeof(unsigned int) * 8 + bit;
-        return reinterpret_cast<T*>(p->data) + entry;
-      }
-    }
-
-    return NULL;
-  }
-
-public:
-  static inline FixedSizePool &getInstance() {
-    static FixedSizePool instance;
-    return instance;
-  }
-
-  FixedSizePool()
-    : numPerPool(NP * sizeof(unsigned int) * 8),
-      totalPoolSize(sizeof(struct Pool) +
-		    numPerPool * sizeof(T) +
-                    NP * sizeof(unsigned int)),
-      numBlocks(0)
-  { newPool(&pool); }
-
-  ~FixedSizePool() {
-    for (struct Pool *curr = pool; curr; ) {
-      struct Pool *next = curr->next;
-      MA::deallocate(curr);
-      curr = next;
-    }
-  }
-
-  T* allocate() {
-    T* ptr = NULL;
-
-    struct Pool *prev = NULL;
-    struct Pool *curr = pool;
-    while (!ptr && curr) {
-      ptr = allocInPool(curr);
-      prev = curr;
-      curr = curr->next;
-    }
-
-    if (!ptr) {
-      newPool(&prev->next);
-      ptr = allocate();
-      // TODO: In this case we should reverse the linked list for optimality
-    }
-    else {
-      numBlocks++;
-    }
-    return ptr;
-  }
-
-  void deallocate(T* ptr) {
-    int i = 0;
-    for (struct Pool *curr = pool; curr; curr = curr->next) {
-      const T* start = reinterpret_cast<T*>(curr->data);
-      const T* end   = reinterpret_cast<T*>(curr->data) + numPerPool;
-      if ( (ptr >= start) && (ptr < end) ) {
-        // indexes bits 0 - numPerPool-1
-        const int indexD = ptr - reinterpret_cast<T*>(curr->data);
-        const int indexI = indexD / ( sizeof(unsigned int) * 8 );
-        const int indexB = indexD % ( sizeof(unsigned int) * 8 );
-#ifndef NDEBUG
-        if ((curr->avail[indexI] & (1 << indexB))) {
-          std::cerr << "Trying to deallocate an entry that was not marked as allocated" << std::endl;
+		*pnew = p;
 	}
+
+	T* allocInPool(struct Pool* p) {
+		if (!p->numAvail) return NULL;
+
+		for (int i = 0; i < NP; i++) {
+			DWORD tmp = 0;
+			DWORD mask = *(DWORD*)&p->avail[i];
+			const bool isNonzero = _BitScanForward(&tmp, mask);
+			const int bit = (int)tmp;
+			if (isNonzero && bit >= 0) {
+				p->avail[i] ^= 1 << bit;
+				--p->numAvail;
+				const int entry = i * sizeof(unsigned int) * 8 + bit;
+				return reinterpret_cast<T*>(p->data) + entry;
+			}
+		}
+
+		return NULL;
+	}
+
+	//std::mutex mutex;
+public:
+	static inline FixedSizePool& getInstance() {
+		static FixedSizePool instance;
+		return instance;
+	}
+
+	FixedSizePool()
+		: numPerPool(NP * sizeof(unsigned int) * 8),
+		totalPoolSize(sizeof(struct Pool) +
+			numPerPool * sizeof(T) +
+			NP * sizeof(unsigned int)),
+		numBlocks(0)
+	{
+		newPool(&pool);
+	}
+
+	~FixedSizePool() {
+		for (struct Pool* curr = pool; curr; ) {
+			struct Pool* next = curr->next;
+			MA::deallocate(curr);
+			curr = next;
+		}
+	}
+
+	T* allocate() {
+		//mutex.lock();
+		T* ptr = NULL;
+		struct Pool* prev = NULL;
+		struct Pool* curr = pool;
+		while (!ptr && curr) {
+			ptr = allocInPool(curr);
+			prev = curr;
+			curr = curr->next;
+		}
+
+		if (!ptr) {
+			newPool(&prev->next);
+			//mutex.unlock();
+			ptr = allocate();
+			//mutex.lock();
+			// TODO: In this case we should reverse the linked list for optimality
+		}
+		else {
+			numBlocks++;
+		}
+		//mutex.unlock();
+		return ptr;
+	}
+
+	void deallocate(T* ptr) {
+		//mutex.lock();
+		int i = 0;
+		for (struct Pool* curr = pool; curr; curr = curr->next) {
+			const T* start = reinterpret_cast<T*>(curr->data);
+			const T* end = reinterpret_cast<T*>(curr->data) + numPerPool;
+			if ((ptr >= start) && (ptr < end)) {
+				// indexes bits 0 - numPerPool-1
+				const int indexD = ptr - reinterpret_cast<T*>(curr->data);
+				const int indexI = indexD / (sizeof(unsigned int) * 8);
+				const int indexB = indexD % (sizeof(unsigned int) * 8);
+#ifndef NDEBUG
+				if ((curr->avail[indexI] & (1 << indexB))) {
+					std::cerr << "Trying to deallocate an entry that was not marked as allocated" << std::endl;
+				}
 #endif
-        curr->avail[indexI] ^= 1 << indexB;
-        curr->numAvail++;
-        numBlocks--;
-        return;
-      }
-      i++;
-    }
-    std::cerr << "Could not find pointer to deallocate" << std::endl;
-    throw(std::bad_alloc());
-  }
+				curr->avail[indexI] ^= 1 << indexB;
+				curr->numAvail++;
+				numBlocks--;
+				//mutex.unlock();
+				return;
+			}
+			i++;
+		}
+		//mutex.unlock();
+		std::cerr << "Could not find pointer to deallocate" << std::endl;
+		throw(std::bad_alloc());
+	}
 
-  /// Return allocated size to user.
-  std::size_t allocatedSize() const { return numBlocks * sizeof(T); }
+	/// Return allocated size to user.
+	std::size_t allocatedSize()
+	{
+		//mutex.lock();
+		auto ret = numBlocks * sizeof(T);
+		//mutex.unlock();
+		return ret;
+	}
 
-  /// Return total size with internal overhead.
-  std::size_t totalSize() const {
-    return numPools() * totalPoolSize;
-  }
+	/// Return total size with internal overhead.
+	std::size_t totalSize() {
+		//mutex.lock();
+		auto ret = numPools() * totalPoolSize;
+		//mutex.unlock();
+		return ret;
+	}
 
-  /// Return the number of pools
-  std::size_t numPools() const {
-    std::size_t np = 0;
-    for (struct Pool *curr = pool; curr; curr = curr->next) np++;
-    return np;
-  }
+	/// Return the number of pools
+	std::size_t numPools() {
+		//mutex.lock();
+		std::size_t np = 0;
+		for (struct Pool* curr = pool; curr; curr = curr->next) np++;
+		//mutex.unlock();
+		return np;
+	}
 
-  /// Return the pool size
-  std::size_t poolSize() const { return totalPoolSize; }
+	/// Return the pool size
+	std::size_t poolSize()
+	{
+		//mutex.lock();
+		auto ret = totalPoolSize;
+		//mutex.unlock();
+		return ret;
+	}
 };
 
 
